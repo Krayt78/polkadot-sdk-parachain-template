74c0ef3badaf221ff544b7927e15d978189cd574
krayt78 <ludovic.domingues96@gmail.com>
2025-05-05 15:35:30 +0200

    added code for aidrop and fmt

--- pallets/custom-pallet/src/lib.rs
+++ pallets/custom-pallet/src/lib.rs
@@ -1,8 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-pub use pallet::*;
+use frame::deps::frame_support::PalletId;
 use frame::prelude::*;
+use frame::traits::Currency;
+use frame::traits::ExistenceRequirement;
+use frame::traits::AccountIdConversion;
 use serde::{self, Deserialize, Deserializer, Serialize, Serializer};
+use frame::deps::sp_io::crypto::secp256k1_ecdsa_recover;
+
+// Re-export all pallet parts, this is needed to properly import the pallet into the runtime.
+pub use pallet::*;
+
+type BalanceOf<T> =
+    <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+pub trait WeightInfo {
+    fn claim() -> Weight;
+    fn register_claim() -> Weight;
+    fn move_claim() -> Weight;
+}
 
 #[derive(
     Clone, Copy, PartialEq, Eq, Encode, Decode, Default, RuntimeDebug, TypeInfo, MaxEncodedLen,
@@ -61,16 +77,234 @@ impl core::fmt::Debug for EcdsaSignature {
     }
 }
 
+/// Converts the given binary data into ASCII-encoded hex. It will be twice the length.
+fn to_ascii_hex(data: &[u8]) -> Vec<u8> {
+    let mut r = Vec::with_capacity(data.len() * 2);
+    let mut push_nibble = |n| r.push(if n < 10 { b'0' + n } else { b'a' - 10 + n });
+    for &b in data.iter() {
+        push_nibble(b / 16);
+        push_nibble(b % 16);
+    }
+    r
+}
+
 #[frame::pallet]
 pub mod pallet {
     use super::*;
-    use frame::prelude::*;
+    use frame::traits::Currency;
 
     #[pallet::pallet]
     pub struct Pallet<T>(_);
 
-    // Configuration trait for the pallet.
     #[pallet::config]
     pub trait Config: frame_system::Config {
+        /// The overarching event type.
+        type RuntimeEvent: From<Event<Self>> + IsType<<Self as frame_system::Config>::RuntimeEvent>;
+
+        /// The currency mechanism.
+        type Currency: Currency<Self::AccountId>;
+
+        /// The prefix used in signed messages.
+        type Prefix: Get<&'static [u8]>;
+
+        /// The origin which may forcibly move a claim.
+        type MoveClaimOrigin: EnsureOrigin<Self::RuntimeOrigin>;
+
+        /// Treasury account Id
+        #[pallet::constant]
+        type PotId: Get<PalletId>;
+
+        /// The weight info for the pallet.
+        type WeightInfo: WeightInfo;
+    }
+
+    #[pallet::storage]
+    #[pallet::getter(fn claims)]
+    pub type Claims<T: Config> = StorageMap<_, Blake2_128Concat, EthereumAddress, BalanceOf<T>>;
+
+    #[pallet::storage]
+    #[pallet::getter(fn total)]
+    pub type Total<T: Config> = StorageValue<_, BalanceOf<T>, ValueQuery>;
+
+    #[pallet::event]
+    #[pallet::generate_deposit(pub(super) fn deposit_event)]
+    pub enum Event<T: Config> {
+        /// Someone claimed some tokens. [who, ethereum_address, amount]
+        Claimed {
+            who: T::AccountId,
+            ethereum_address: EthereumAddress,
+            amount: BalanceOf<T>,
+        },
+        /// Someone's claim was moved to a new address. [old, new]
+        ClaimMoved {
+            old: EthereumAddress,
+            new: EthereumAddress,
+        },
+    }
+
+    #[pallet::error]
+    pub enum Error<T> {
+        /// Invalid Ethereum signature.
+        InvalidEthereumSignature,
+        /// Ethereum address has no claim.
+        SignerHasNoClaim,
+        /// There's not enough in the pot to pay out some unvested amount. Generally implies a logic error.
+        InsufficientPalletBalance,
+    }
+
+    #[pallet::call]
+    impl<T: Config> Pallet<T> {
+        /// Make a claim to collect your tokens.
+        ///
+        /// The dispatch origin for this call must be _None_.
+        ///
+        /// Unsigned Validation:
+        /// A call to claim is deemed valid if the signature provided matches
+        /// the expected signed message of:
+        ///
+        /// > Ethereum Signed Message:
+        /// > (configured prefix string)(address)
+        ///
+        /// and `address` matches the `dest` account.
+        ///
+        /// Parameters:
+        /// - `dest`: The destination account to payout the claim.
+        /// - `ethereum_signature`: The signature of an ethereum signed message matching the format
+        ///   described above.
+        ///
+        /// <weight>
+        /// The weight of this call is invariant over the input parameters.
+        /// Weight includes logic to validate unsigned `claim` call.
+        ///
+        /// Total Complexity: O(1)
+        /// </weight>
+        #[pallet::call_index(0)]
+        #[pallet::weight(T::WeightInfo::claim())]
+        pub fn claim(
+            origin: OriginFor<T>,
+            dest: T::AccountId,
+            ethereum_signature: EcdsaSignature,
+        ) -> DispatchResult {
+            ensure_none(origin)?;
+
+            let data = dest.using_encoded(to_ascii_hex);
+            let signer = Self::eth_recover(&ethereum_signature, &data, &[][..])
+                .ok_or(Error::<T>::InvalidEthereumSignature)?;
+
+            Self::process_claim(signer, dest)?;
+            Ok(())
+        }
+
+        /// Register a new claim to collect tokens from the pallet account.
+        ///
+        /// The dispatch origin for this call must be _Root_.
+        ///
+        /// Parameters:
+        /// - `who`: The Ethereum address allowed to collect this claim.
+        /// - `value`: The number of tokens that will be claimed.
+        #[pallet::call_index(1)]
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+        pub fn register_claim(
+            origin: OriginFor<T>,
+            who: EthereumAddress,
+            value: BalanceOf<T>,
+        ) -> DispatchResult {
+            ensure_root(origin)?;
+
+            ensure!(
+                T::Currency::free_balance(&Self::account_id()) >= value,
+                Error::<T>::InsufficientPalletBalance
+            );
+
+            Claims::<T>::insert(who, value);
+            Total::<T>::mutate(|t| *t = t.saturating_add(value));
+
+            Ok(())
+        }
+
+        /// Move a claim to a new address.
+        ///
+        /// The dispatch origin for this call must be _Root_.
+        ///
+        /// Parameters:
+        /// - `old`: The old Ethereum address.
+        /// - `new`: The new Ethereum address.
+        #[pallet::call_index(2)]
+        #[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1).ref_time())]
+        pub fn move_claim(
+            origin: OriginFor<T>,
+            old: EthereumAddress,
+            new: EthereumAddress,
+        ) -> DispatchResultWithPostInfo {
+            T::MoveClaimOrigin::try_origin(origin)
+                .map(|_| ())
+                .or_else(ensure_root)?;
+
+            Claims::<T>::take(&old).map(|c| Claims::<T>::insert(&new, c));
+
+            Self::deposit_event(Event::ClaimMoved { old, new });
+            Ok(Pays::No.into())
+        }
+    }   
+}
+
+impl<T: Config> Pallet<T> {
+    /// The account ID of the pallet.
+    pub fn account_id() -> T::AccountId {
+        T::PotId::get().into_account_truncating()
+    }
+
+    /// Constructs the message that Ethereum RPC's `personal_sign` and `eth_sign` would sign.
+    fn ethereum_signable_message(what: &[u8], extra: &[u8]) -> Vec<u8> {
+        let prefix = T::Prefix::get();
+        let mut l = prefix.len() + what.len() + extra.len();
+        let mut rev = Vec::new();
+        while l > 0 {
+            rev.push(b'0' + (l % 10) as u8);
+            l /= 10;
+        }
+        let mut v = b"\x19Ethereum Signed Message:\n".to_vec();
+        v.extend(rev.into_iter().rev());
+        v.extend_from_slice(prefix);
+        v.extend_from_slice(what);
+        v.extend_from_slice(extra);
+        v
+    }
+
+    // Attempts to recover the Ethereum address from a message signature signed by using
+    // the Ethereum RPC's `personal_sign` and `eth_sign`.
+    fn eth_recover(s: &EcdsaSignature, what: &[u8], extra: &[u8]) -> Option<EthereumAddress> {
+        let msg = keccak_256(&Self::ethereum_signable_message(what, extra));
+        let mut res = EthereumAddress::default();
+        res.0
+            .copy_from_slice(&keccak_256(&secp256k1_ecdsa_recover(&s.0, &msg).ok()?[..])[12..]);
+        Some(res)
+    }
+
+    fn process_claim(signer: EthereumAddress, dest: T::AccountId) -> DispatchResult {
+        let balance_due = Claims::<T>::get(&signer).ok_or(Error::<T>::SignerHasNoClaim)?;
+
+        let new_total = Total::<T>::get()
+            .checked_sub(&balance_due)
+            .ok_or(Error::<T>::InsufficientPalletBalance)?;
+
+        // Transfer tokens from pallet account to the destination account
+        let pallet_account = Self::account_id();
+        T::Currency::transfer(
+            &pallet_account,
+            &dest,
+            balance_due,
+            ExistenceRequirement::KeepAlive,
+        )?;
+
+        Total::<T>::put(new_total);
+        Claims::<T>::remove(&signer);
+
+        Self::deposit_event(Event::Claimed {
+            who: dest,
+            ethereum_address: signer,
+            amount: balance_due,
+        });
+        Ok(())
     }
 }
